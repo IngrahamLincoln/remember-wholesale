0c4c2e58ba71c4f1957218f2c1a95b8ab667521f
IngrahamLincoln <ingrami@sou.edu>
2023-11-03 18:57:23 -0700

    a little disorganized on file management. implementing a way to condense the context. slow going.

--- MVP.py
+++ MVP.py
@@ -2,13 +2,29 @@ import sys
 import os
 from dotenv import load_dotenv
 import pickle
+import tiktoken
 from langchain.chat_models import ChatOpenAI
 from langchain.schema import SystemMessage, HumanMessage, AIMessage
+from langchain.callbacks import get_openai_callback
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 
 # Load environment variables
 load_dotenv()
 api_key = os.getenv('OPEN_API_KEY')
+
+tokenizer = tiktoken.get_encoding('cl100k_base')
+
+#length function for tokenizer
+def tiktoken_len(conversation):
+    total_tokens = 0
+    for message in conversation:
+        tokens = tokenizer.encode(
+            message['content'],
+            disallowed_special=()
+        )
+        total_tokens += len(tokens)
+    return total_tokens
+
 
 class Agent:
     def __init__(self, name, prompt, history_file="history.pkl"):
@@ -22,11 +38,16 @@ class Agent:
         self.message_history.insert(0, SystemMessage(content=prompt))
 
     def message(self, user_message):
-        self.message_history.append(HumanMessage(content=user_message))
-        resp = self.chat(self.message_history)
-        print("\n")
-        self.message_history.append(resp)
-        self.save_conversation()
+        with get_openai_callback() as cb:  # Use the context manager to count tokens
+            self.message_history.append(HumanMessage(content=user_message))
+            resp = self.chat(self.message_history)
+            self.message_history.append(resp)
+            self.save_conversation()
+
+        # Access the total tokens used from the context manager
+        #total_tokens = cb.total_tokens
+        #print(f"Total tokens used for this message: {total_tokens}")
+
         return resp.content
 
     def save_conversation(self):
@@ -40,27 +61,28 @@ class Agent:
         else:
             return []
 
-def save_to_text_file(filename, conversation):
+def save_to_text_file(filename, conversation, token_count=None):
     with open(filename, 'w') as f:
         for message in conversation:
             f.write(f"{message['name']}: {message['content']}\n")
+        if token_count is not None:
+            f.write(f"Total tokens: {token_count}\n")
 
 def run_conversation(agent, text_filename="conversation.txt"):
     conversation = []
     while True:
         user_message = input("You: ")
         agent_response = agent.message(user_message)
-
+        print('\n')
         conversation.append({"name": "User", "content": user_message})
         conversation.append({"name": "Agent", "content": agent_response})
         
-        save_to_text_file(text_filename, conversation)
+        # Calculate the total tokens for the conversation
+        token_count = tiktoken_len(conversation)
         
-        print(f"{agent.name}: {agent_response}")
+        # Save the conversation with the latest token count appended
+        save_to_text_file(text_filename, conversation, token_count)
 
-        cont = input("Continue conversation? (y/n): ")
-        if cont.lower() != 'y':
-            break
 
 if __name__ == "__main__":
     # Initialize the agent
--- iter3.py
+++ iter3.py
@@ -2,6 +2,7 @@ import sys
 import os
 from dotenv import load_dotenv
 import json
+import uuid
 from datetime import datetime
 import tiktoken
 from langchain.chat_models import ChatOpenAI
@@ -45,19 +46,27 @@ class Agent:
             self.message_history.append(resp)
             self.save_conversation()
 
-        # Access the total tokens used from the context manager
-        #total_tokens = cb.total_tokens
-        #print(f"Total tokens used for this message: {total_tokens}")
-
         return resp.content
 
     def save_conversation(self):
         with open(self.history_file, 'w') as f:
             # Convert Message objects to dictionaries and save as JSON
-            history = [{'name': 'System', 'content': message.content} if isinstance(message, SystemMessage)
-                    else {'name': 'User', 'content': message.content} if isinstance(message, HumanMessage)
-                    else {'name': 'AI', 'content': message.content} 
-                    for message in self.message_history]
+            history = []
+            for message in self.message_history:
+                # Determine the type of message and construct the dictionary
+                if isinstance(message, SystemMessage):
+                    message_dict = {'name': 'System', 'content': message.content}
+                elif isinstance(message, HumanMessage):
+                    message_dict = {'name': 'User', 'content': message.content}
+                else:  # Assuming AIMessage for simplicity
+                    message_dict = {'name': 'AI', 'content': message.content}
+                
+                # Add the token count to the message dictionary
+                message_dict['tokens'] = tiktoken_len([message_dict])
+                
+                # Append the message dictionary to the history list
+                history.append(message_dict)
+                
             json.dump(history, f, indent=4)
 
     def load_conversation(self):
@@ -76,15 +85,7 @@ class Agent:
         else:
             return []
 
-def append_to_json_file(filename, message, author, token_count):
-    # Create a dictionary with the message data
-    message_data = {
-        "author": author,
-        "datetime": datetime.now().isoformat(),
-        "message": message,
-        "tokens": token_count
-    }
-    
+def append_to_json_file(filename, message_data):
     # Check if the file exists and contains data
     if os.path.exists(filename) and os.path.getsize(filename) > 0:
         with open(filename, 'r+') as f:
@@ -105,8 +106,62 @@ def append_to_json_file(filename, message, author, token_count):
 def clear_file(filename):
     open(filename, 'w').close()
 
-def run_conversation(agent, json_filename="conversation.json"):
-    clear_file(json_filename)  # Clear the conversation file
+
+
+def chunk_and_summarize(filename="history.json"):
+    # Load the message history
+    with open(filename, 'r') as f:
+        messages = json.load(f)
+
+    # Filter out messages that have been summarized or have empty content
+    filtered_messages = [msg['content'] for msg in messages if not msg.get('summarized', False) and msg['content'].strip()]
+
+    # Concatenate all the content fields into a single string
+    all_content = ' '.join(filtered_messages)
+
+    # Update the 'summarized' status for all messages that have been concatenated
+    for msg in messages:
+        if not msg.get('summarized', False) and msg['content'].strip():
+            msg['summarized'] = True
+
+    # Save the updated messages back to the file
+    with open(filename, 'w') as f:
+        json.dump(messages, f, indent=4)
+
+    return all_content
+
+
+
+def summarize_chunk(agent, chunk):
+    # Get the summary from the agent
+    summary = agent.message(chunk)
+    return summary
+
+def save_summary(summary):
+    # Each summary is stored with a timestamp
+    summary_data = {
+        "id": str(uuid.uuid4()),
+        "datetime": datetime.now().isoformat(),
+        "name": "summarizer agent",
+        "summary": summary,
+    }
+    return summary_data
+
+def save_message(message, author, token_count):
+    message_id = str(uuid.uuid4())
+    # Create a dictionary with the message data
+    message_data = {
+        "id": message_id,
+        "name": author,
+        "datetime": datetime.now().isoformat(),
+        "message": message,
+        "tokens": token_count,
+        "summarized": False 
+    }
+    return message_data
+
+def run_conversation(agent, summarizer_agent, convo_filename="conversation.json", summary_file="summarizer_history.json"):
+    clear_file(convo_filename)  # Clear the conversation file
     conversation = []
     while True:
         user_message = input("You: ")
@@ -118,14 +173,24 @@ def run_conversation(agent, json_filename="conversation.json"):
         conversation.append({"name": "User", "content": user_message, "tokens": user_token_count})
         
         # Append the user message to the JSON file
-        append_to_json_file(json_filename, user_message, "User", user_token_count)
+        user_message_data = save_message(user_message, "User", user_token_count)
+        append_to_json_file(convo_filename, user_message_data)
         
         # Get token count for the agent response
         agent_token_count = tiktoken_len([{"content": agent_response}])
         conversation.append({"name": "Agent", "content": agent_response, "tokens": agent_token_count})
         
         # Append the agent response to the JSON file
-        append_to_json_file(json_filename, agent_response, "Agent", agent_token_count)
+        agent_message_data = save_message(agent_response, "Agent", agent_token_count)
+        append_to_json_file(convo_filename, agent_message_data)
+
+        chunk = chunk_and_summarize()
+        if chunk:
+            summary = summarize_chunk(summarizer_agent, chunk)
+            summary_data = save_summary(summary)
+            append_to_json_file(summary_file, summary_data)
+
+
 
 if __name__ == "__main__":
     # Initialize the agent
@@ -137,4 +202,4 @@ if __name__ == "__main__":
         summarizer_sys_prompt = f.read()
     summarizer_agent = Agent("SummarizerAgent", summarizer_sys_prompt, history_file="summarizer_history.json")
     # Run the conversation
-    run_conversation(agent)
+    run_conversation(agent, summarizer_agent)
